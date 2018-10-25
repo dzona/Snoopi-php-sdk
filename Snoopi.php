c382207b4ada1e9872ae87a1469258cd7d18f602
Carlos Arias <carlos@hirocreative.com>
2018-10-25 08:17:36 -0500

    Added Comments
    Added comments and basic usage for each method.

--- Snoopi.php
+++ Snoopi.php
@@ -40,6 +40,13 @@ public function send_request($url, $data="") {
 }
 		
 	
+    /**
+     * @param array|null $params
+     *
+     * @basic usage GeoIPLocation('186.83.228.58')
+	 *
+     * @return JSON.
+     */
 
 	function GeoIPLocation($IPAddress, $params="") {
 		$url = "{$this->url}/ip/{$IPAddress}";
@@ -48,6 +55,15 @@ public function send_request($url, $data="") {
 	}
 
 
+    /**
+     * @param array|null $params
+     *
+     * @fromZip toZip numeric value (55555)
+     *
+     * @basic usage ZipCodeDistance('33426', '33433')
+	 *
+     * @return JSON.
+     */
 
 	function ZipCodeDistance($fromZip, $ToZip, $params="") {
 		$url = "{$this->url}/zipcodedistance/{$fromZip}-{$ToZip}";
@@ -56,7 +72,15 @@ public function send_request($url, $data="") {
 		return $Response;
 	}
 
-	
+    /**
+     * @param array|null $params
+     * 
+     * @str string abreviated state
+     *
+     * @basic usage StateInfo('FL')
+	 *
+     * @return JSON.
+     */
 	function StateInfo($str, $params="") {
 		$data = trim($data);
 		$data = urlencode($data);
@@ -65,7 +89,16 @@ public function send_request($url, $data="") {
 		$Response = $this->send_request($url, $params); 
 		return $Response;
 	}
-	
+
+    /**
+     * @param array|null $params
+     * 
+     * @str string abreviated state
+     *
+     * @basic usage GetCities('FL')
+	 *
+     * @return JSON.
+     */
 	function GetCities($str, $params="") {
 		$data = trim($data);
 		$data = urlencode($data);
@@ -74,20 +107,24 @@ public function send_request($url, $data="") {
 		$Response = $this->send_request($url, $params); 
 		return $Response;
 	}
-	
+
+    /**
+     * @param array|null $params
+     * 
+     * @FromZip numeric (55555)
+	 *
+	 * @Radius numeric (miles) 10 is default 
+     *
+     * @basic usage ZipCodeRadius('33426', 10)
+	 *
+     * @return JSON.
+     */
 	function ZipCodeRadius($FromZip, $Radius, $params="") {
 		$url = "{$this->url}/zipcoderange/{$FromZip}-{$Radius}";
 		$Response = $this->send_request($url, $params); 
 		return $Response;
 	}
 
-    function ReverseGeo($str, $params=""){
-		$data = urlencode($data);
-        $url = "{$this->url}/reversegeo/{$data}";
-
-        $Response = $this->send_request($url, $params); 
-        return $Response;
-    }
  
 
 } // End Class
